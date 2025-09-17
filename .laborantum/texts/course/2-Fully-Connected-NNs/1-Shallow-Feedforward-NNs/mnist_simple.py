bca8e39ba115f11a8560a9a1ed542d59160ecf97
k-berber <berberkirill@mail.ru>
2025-09-17 17:18:50 +0000

    some changes

--- .laborantum/texts/course/2-Fully-Connected-NNs/1-Shallow-Feedforward-NNs/mnist_simple.py
+++ .laborantum/texts/course/2-Fully-Connected-NNs/1-Shallow-Feedforward-NNs/mnist_simple.py
@@ -1,23 +1,33 @@
 import torchvision.datasets
+from torchvision import transforms
+import torch
 
 class MNISTSimpleDataset:
     def __init__(self, train=True):
-        ...
-        ## Load MNIST dataset here
-        ## YOUR CODE HERE
-
-
-    def __len__(self):
-        res = 0
-        ## Return number of items that is there in the dataset
-        ## YOUR CODE HERE
-        return res
-
-
-    def __getitem__(self, index):
-        sample = {}
-
-        ## Return a sample of the dataset that correponds to the input index
-        ## YOUR CODE HERE
+        # Используем стандартный MNIST датасет из torchvision
+        # Он автоматически использует уже скачанные файлы из data/MNIST/raw/
+        self.dataset = torchvision.datasets.MNIST(
+            root='./data',          # Папка, где лежат данные
+            train=train,            # Выбираем тренировочный или тестовый набор
+            download=False,         # Не скачиваем, т.к. файлы уже есть
+            transform=transforms.Compose([
+                transforms.ToTensor(),  # Конвертируем в тензоры
+                transforms.Normalize((0.1307,), (0.3081,))  # Нормализуем
+            ])
+        )
         
-        return sample 
+    def __len__(self):
+        # Возвращаем количество элементов в датасете
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        # Получаем данные из стандартного датасета (возвращает кортеж)
+        image, label = self.dataset[index]
+        label_tensor = torch.tensor(label, dtype=torch.int64)
+        # Создаем sample в виде словаря
+        sample = {
+            'image': image,
+            'label': label_tensor
+        }
+        
+        return sample
